cef163f9a38d4c9107c61db7a25a807c0e3d3a46
Joshua Weinstein <jweinstein@splunk.com>
2019-12-02 15:20:12 -0800

    prelim changes

--- setup.py
+++ setup.py
@@ -1,17 +1,31 @@
 from distutils.core import setup, Extension
 
-# A Python package may have multiple extensions, but this
-# template has one.
-module1 = Extension('DemoPackage',
+keyword_list = [
+    'finance',
+    'stocks',
+    'modeling',
+    'stock market'
+]
+
+classifers_list = [
+    'Development Status :: 2 - Pre-Alpha',
+    'Operating System :: MacOS',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: Unix'
+]
+
+module1 = Extension('_royal',
                     define_macros = [('USE_PRINTER', '1')],
                     include_dirs = ['include'],
                     sources = ['src/demo.c'])
 
-setup (name = 'DemoPackage',
-       version = '1.0',
+setup (name = 'royal',
+       version = '0.0.1',
        description = 'This is a demo package',
-       author = '<first> <last>',
-       author_email = 'person@site.com',
-       url = 'https://docs.python.org/extending/building',
+       author = 'Joshua Weinstein',
+       author_email = 'jweinst1@berkeley.edu',
+       url = 'https://github.com/jweinst1/Royal',
        long_description = open('README.md').read(),
+       keyowrds = keyword_list,
+       classifiers = classifers_list,
        ext_modules = [module1])
